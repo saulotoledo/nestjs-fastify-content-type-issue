e7a2237db2b30dc82c6e35d4ba1b0dc06e093b40
Saulo <saulotoledo@gmail.com>
2020-05-31 10:44:35 -0300

    Adding custom content-type parser

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,16 @@ import {
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const adapter = new FastifyAdapter();
+
+  // Sample content-type parser. In production see https://github.com/fastify/fastify/blob/2.x/lib/contentTypeParser.js
+  adapter.getInstance().addContentTypeParser('application/hal+json', (req, body, done) => {
+    done(null, JSON.stringify(body));
+  });
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    adapter
   );
   await app.listen(3000);
 }
